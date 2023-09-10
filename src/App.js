ff65f0b3596a6775b225ac4c12a5ed6a37a07ad1
Elena <helena151612@gmail.com>
2023-09-10 11:17:02 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Olena Mazurenko and is{" "}
-        <a
-          href="https://github.com/ElenaMaz/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://vocal-tapioca-489c64.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Olena Mazurenko and is{" "}
+          <a
+            href="https://github.com/ElenaMaz/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://vocal-tapioca-489c64.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
