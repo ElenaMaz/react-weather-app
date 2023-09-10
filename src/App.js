e3cd3ee72ef85e03687c88f40c6a3a7a4b3e259f
Elena <helena151612@gmail.com>
2023-09-10 19:15:59 +0300

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
-        <footer>
-          This project was coded by Olena Mazurenko and is{" "}
-          <a
-            href="https://github.com/ElenaMaz/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on Github
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://vocal-tapioca-489c64.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify{" "}
-          </a>
-        </footer>
+        <Weather />
       </div>
+      <footer>
+        This project was coded by Olena Mazurenko and is{" "}
+        <a
+          href="https://github.com/ElenaMaz/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://vocal-tapioca-489c64.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleresponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      date: "Sunday 17:02",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h3>{weatherData.city}</h3>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            {Math.round(weatherData.temperature)}°C{" "}
+            <img src={weatherData.icon} alt={weatherData.description} />
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 0%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "62bc298785543e137bc6756e514eb1c3";
+    let city = "Cherkasy";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleresponse);
+  }
+}
